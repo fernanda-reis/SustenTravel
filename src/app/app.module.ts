bc18db4df7337defa188ddf41ce2525f285f0721
Fernanda <fernandasoares.reis1@gmail.com>
2022-02-21 15:22:56 -0300

    @fernanda-reis [Implementação rodapé e inicialização aba Contato]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RodapeComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
